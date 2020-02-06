3c4669a695c3676bc9fe6cb58fdb086de6d968d8
Taha Nemat <tahanemat6@gmail.com>
2020-02-06 21:04:47 +0500

    added 9th answer

---
+++ 9.js
@@ -0,0 +1,25 @@
+// Special Pythagorean triplet
+   
+// Problem 9
+
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+// a^2 + b^2 = c^2
+
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+
+// Find the product abc.
+
+for (let a = 1; a < 1000; a++){
+    for(let b = a; b < 1000; b++){
+        let c = Math.sqrt((a*a) + (b*b))
+        if (a+b+c > 1000){
+            break;
+        }
+        else if (a+b+c === 1000){
+            console.log(`${a}, ${b}, ${c}`); // Answer: (200, 375, 425)
+        }
+    }
+}
